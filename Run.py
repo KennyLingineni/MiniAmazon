adcd5c2ca4219fee32d08ad0f51e577138e371b2
Kenny <kenny.lingineni@gmail.com>
2018-06-18 03:41:24 +0530

    Made Some Changes

--- Run.py
+++ Run.py
@@ -1,20 +1,40 @@
 import flask
-from flask import Flask
-from flask import request
-app=Flask('temp')
+from flask import Flask,request,jsonify,send_from_directory
 
-# http://127.0.0.1:5005/say_hello?name=Kenny
-@app.route('/say_hello',methods=['GET','POST'])
-def say_hello():
+app=Flask('MiniAmazon')
+
+products=[]
+
+@app.route('/',methods=['GET'])
+#This api is to return the index or home page of the portal
+def index():
+    print("Entered Index")
+    return send_from_directory('static', 'Index.html')
+
+@app.route('/api/product',methods=['GET','POST'])
+def product():
+    print("Enters the def for searrch with GET")
     if request.method == 'GET':
-        return 'HELLO ' + request.args['name']
+        query = request.args['name']
+        for prod in products:
+            if prod['name'] == query:
+                print("From GET query is for: ",query)
+                return jsonify(prod)
+        return 'NO Match'
+
     elif request.method == 'POST':
-        return '[POST] HELLO ' + request.form['name'] + ' age ' + request.form['age']
+        print("Enters the def to update with POST")
+        name = request.form['name']
+        desc = request.form['desc']
+        price = request.form['price']
+        prod = {
+            'name': name,
+            'desc': desc,
+            'price': price
+        }
+        print(prod)
+        products.append(prod)
+        return send_from_directory('static,index.html')
 
-@app.route('/say_Bye',methods=['GET'])
-def say_bye():
 
-    return 'BYE'
-
-app.run(host='127.0.0.1',port=5009)
-
+app.run(host='127.0.0.1', port=5009, debug=True)
